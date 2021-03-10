64ca09bd63a1fb3754db602a99bf28c170aa754b
Jake Melville <jakemelville@ymail.com>
2021-03-09 19:51:20 -0500

    changed structure of functions. NOw the user is initally prompted to either view, add or update. Created functions for each of the 3 choices. Organized in a switch function

--- server.js
+++ server.js
@@ -2,41 +2,86 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const connection = require('./db/connection');
 
-async function addData() {
+async function inputData() {
     const dataChoice = await inquirer.prompt(
         {
             type: 'list',
             name: 'addingInfo',
-            message: 'What would you like to add?',
-            choices: ['Department', 'Role', 'Employee']
+            message: 'What would you like to do?',
+            choices: ['Add', 'View', 'Update']
         }
     )
     switch(dataChoice.addingInfo) {
-        case 'Department':
-            const departmentInfo = await department();
+        case 'Add':
+            const add = await addInfo();
             break;
-        case 'Role':
-            const roleInfo = await role();
+        case 'View':
+            const view = await viewInfo();
             break;
-        case 'Employee':
-            const employeeInfo = await employee();
+        case 'Update':
+            const update = await updateInfo();
             break;
     }
 }
-async function department() {
-    const depChoices = await inquirer.prompt(
+async function addInfo() {
+    const addChoices = await inquirer.prompt(
         {
             type: 'list',
-            name: 'depOptions',
-            message: 'Would you like to add, update, or view current departments?',
-            choices: ['Add', 'Update', 'View']
+            name: 'addOptions',
+            message: 'Would you like to add a department, role, or employee?',
+            choices: ['Department', 'Role', 'Employee']
         }
     )
-    switch (depChoices.depOptions) {
-        case 'Add':
-            console.log("choice add selected");
+    switch (addChoices.addOptions) {
+        case 'Department':
+            console.log("add department");
             break;
-
+        case 'Role':
+            console.log("add role");
+            break;
+        case 'Employee':
+            console.log("add employee");
+            break;
     }
 }
-addData();
+async function viewInfo() {
+    const viewChoices = await inquirer.prompt(
+        {
+            type: 'list',
+            name: 'viewOptions',
+            message: 'Would you like to view departments, roles, or employees?',
+            choices: ['Departments', 'Roles', 'Employees']
+        }
+    )
+    switch (viewChoices.viewOptions) {
+        case 'Departments':
+            console.log("view departments");
+            break;
+        case 'Roles':
+            console.log("view roles");
+            break;
+        case 'Employees':
+            console.log("view employees");
+            break;
+    }
+}
+async function updateInfo() {
+    const updateChoices = await inquirer.prompt(
+        {
+            type: 'list',
+            name: 'updateOptions',
+            message: 'Would you like to update employees or roles?',
+            choices: ['Employees', 'Roles']
+        }
+    )
+    switch (updateChoices.updateOptions) {
+        case 'Employees':
+            console.log("update employees");
+            break;
+        case 'Roles':
+            console.log("update roles");
+            break;
+    }
+}
+
+inputData();
